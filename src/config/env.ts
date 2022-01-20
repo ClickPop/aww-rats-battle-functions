9eeb4e05d78fe0a6e5b7ef88d696cb66aa143444
Graham Vasquez <rescus1221@gmail.com>
2022-01-19 22:30:50 -0500

    Added env and graphql lib from tahmid's PR to make my life a little easier

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,3 +9,4 @@ export const PLAYER_BASE_ENERGY = 20;
 export const PORT = process.env.PORT ?? 5002;
 export const HASURA_BASE_URL =
   process.env.HASURA_BASE_URL ?? 'http://localhost:8080';
+export const HASURA_ADMIN_SECRET = process.env.HASURA_ADMIN_SECRET ?? '';
